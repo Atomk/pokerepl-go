ea94b7dcf80c911f1ea4579aa688dca665ce671f
Atomk <marco.fras@hotmail.it>
2026-05-08 00:20:11 +0200

    refactor: move commands to separate file
    Now all commands live in their own file, separate from the REPL. I added the function NewContext to abstract that implementation detail, the application entry point does not need to know how to setup the context in the way that commands expect.

---
+++ commands.go
@@ -0,0 +1,124 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"os"
+	"strings"
+	"time"
+
+	pokecache "github.com/atomk/pokedexcli/internal"
+)
+
+// Contains URLs used for pagination.
+// If both previous and Next are null, no request was made to get location areas.
+type Context struct {
+	cache    *pokecache.Cache
+	Previous *string
+	Next     *string
+}
+
+func NewContext(minutes uint) *Context {
+	return &Context{
+		cache: pokecache.NewCache(time.Duration(minutes) * time.Minute),
+	}
+}
+
+func commandMapNext(context *Context, args []string) error {
+	if context.Previous != nil && context.Next == nil {
+		fmt.Println("you're on the last page")
+		return nil
+	}
+
+	result, err := getLocationAreas(context.Next, context.cache)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	context.Previous = result.Previous
+	context.Next = result.Next
+
+	if len(result.Results) == 0 {
+		fmt.Println("No locations found")
+		return nil
+	}
+
+	for _, location := range result.Results {
+		fmt.Println(location.Name)
+	}
+
+	return nil
+}
+
+func commandMapPrevious(context *Context, args []string) error {
+	if context.Next != nil && context.Previous == nil {
+		fmt.Println("you're on the first page")
+		return nil
+	}
+
+	result, err := getLocationAreas(context.Previous, context.cache)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	context.Previous = result.Previous
+	context.Next = result.Next
+
+	if len(result.Results) == 0 {
+		fmt.Println("No locations found")
+		return nil
+	}
+
+	for _, location := range result.Results {
+		fmt.Println(location.Name)
+	}
+
+	return nil
+}
+
+func commandExplore(context *Context, args []string) error {
+	if len(args) != 1 {
+		return fmt.Errorf("command `explore` requires exactly one argument")
+	}
+
+	idOrName := strings.TrimSpace(args[0])
+	if len(idOrName) == 0 {
+		return fmt.Errorf("provided argument is an empty string")
+	}
+
+	result, err := getLocationArea(idOrName, context.cache)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	if len(result.PokemonEncounters) == 0 {
+		fmt.Println("no pokemon in this area")
+		return nil
+	}
+
+	for _, encounter := range result.PokemonEncounters {
+		fmt.Printf("- %s\n", encounter.Pokemon.Name)
+	}
+
+	return nil
+}
+
+func commandHelp(context *Context, args []string) error {
+	_, err := fmt.Println("Welcome to the Pokedex!\nUsage:")
+	if err != nil {
+		return err
+	}
+	for _, val := range COMMANDS {
+		fmt.Printf("  %s: %s\n", val.name, val.description)
+	}
+	return nil
+}
+
+func commandExit(context *Context, args []string) error {
+	_, err := fmt.Println("Closing the Pokedex... Goodbye!")
+	if err != nil {
+		return err
+	}
+	os.Exit(0)
+	return nil
+}
